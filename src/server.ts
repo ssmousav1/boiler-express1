c6d3870f2c552090313337bc15141b12658bc7db
Sobhan Mousavi <sobhanmousavi121122@yahoo.com>
2021-07-20 18:22:50 +0430

    server.js to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-const express = require( "express" );
+import express from "express";
 const app = express();
-const port = 8080; // default port to listen
+const port = 3005; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
